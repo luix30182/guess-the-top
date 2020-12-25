3bfbd86164e35dc5bb87a354f47a14ac4c9b48c8
Mario <luix30lp@gmail.com>
2020-12-25 15:20:55 -0600

    Update dashboard game style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'guess-the-top';
+  answers = [
+    {
+      ans: 'manzana',
+      score: 7
+    }
+    , {
+      ans: 'pera',
+      score: 21
+    },
+    {
+      ans: 'mango',
+      score: 16
+    }
+    , {
+      ans: 'fresa',
+      score: 11
+    }, {
+      ans: 'sandia',
+      score: 14
+    }
+
+  ]
 }
